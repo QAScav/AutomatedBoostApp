3439c14d1367cc3fe64c3222e14734cd5ca7ccea
QAScav <80210805+QAScav@users.noreply.github.com>
2023-11-08 10:44:20 +0100

    Fixed sendKeys Added Click function Fixed setup Fixed teardown Added JoinComp Fixed Open Challenge
    CHECK THAT MSEDGEDRIVER IS THE SAME VERSION AS YOUR EDGE BROWSER

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -5,3 +5,7 @@ S
 #src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
 V
 &src/test/java/SeleniumAutomations.java,1\3\13f89a862ebf34aa59663f2173163d8acbf05f78
+V
+&src/test/java/seleniumAutomations.java,b\5\b590808882f14d8bc97a92bc4f702c5604a4d1df
+Q
+!src/test/java/sampleCodeTest.java,a\9\a933e03f1b4697c0581b7aa169d7a05fa1c22978
---
+++ src/test/java/seleniumAutomations.java
@@ -0,0 +1,65 @@
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class seleniumAutomations {
+    WebDriver driver;
+    WebDriverWait wait;
+
+
+    public void sendKeys(WebDriver driver, By by, String text) {
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        element.sendKeys(text);
+    }
+    public void waitForTextInElement(By locator, String expectedText) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.textToBePresentInElementLocated(locator, expectedText));
+    }
+    public void Click(WebDriver driver, By by){
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
+        element.click();
+    }
+
+    @Before
+    public void setup(){
+        System.setProperty("webdriver.edge.driver", "C:\\Selenium\\msedgedriver.exe");
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--remote-allow-origins=*", "--ignore-certificate-errors");
+        driver = new EdgeDriver(edgeOptions);
+        driver.manage().window().maximize();
+        driver.get("https://nexerboostappadmin.azurewebsites.net/home");
+        waitForTextInElement(By.className("ng-star-inserted"), "Jesper Eriksson");
+    }
+    @After
+    public void teardown(){
+        driver.quit();
+    }
+    @Test
+    public void OpenChallenges() {
+        Click(driver, By.cssSelector(".mat-list > a:nth-child(3)"));
+    }
+
+    @Test
+    public void JoinComp(){
+        Click(driver, By.cssSelector(".mat-list > a:nth-child(2)"));
+        Click(driver, By.cssSelector(".container > div:nth-child(1) > div:nth-child(1) > app-competition-view:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)"));
+        Click(driver, By.cssSelector("div.team:nth-child(1) > div:nth-child(1) > div:nth-child(2) > button:nth-child(2)"));
+        Click(driver, By.cssSelector("button.mat-focus-indicator:nth-child(3)"));
+        Click(driver, By.cssSelector(".modal-footer > mat-button:nth-child(1)"));
+        Click(driver, By.cssSelector("button.mat-focus-indicator:nth-child(3)"));
+    }
+}
