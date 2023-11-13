3bb138677bb12bfc41955795d4580918e5fd3e1c
QAScav <80210805+QAScav@users.noreply.github.com>
2023-11-13 14:47:28 +0100

    First version of BAfuncFindingTeam Imported tests

--- src/test/java/seleniumAutomations.java
+++ src/test/java/seleniumAutomations.java
@@ -17,32 +17,56 @@ public class seleniumAutomations {
     WebDriverWait wait;
 
 
-
     public void sendKeys(WebDriver driver, By by, String text) {
+        //Combines wait, locator and text to send in a single line of code
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
         element.sendKeys(text);
     }
     public void waitForTextInElement(By locator, String expectedText) {
+        //As of now the site refreshes automatically once because it first loads without a logged in user,
+        // and then with whatever user is cached, this function is used ONCE in the very start to verify that the site
+        // is done refreshing before continuing with the rest of the tests.
+
+        // "expectedText" NEEDS TO BE CHANGED TO MATCH YOUR ACCOUNT NAME OTHERWISE IT WILL GET STUCK
+        // might be able to make this generic depending on if there's text there at all pre-refresh
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.textToBePresentInElementLocated(locator, expectedText));
     }
     public void click(WebDriver driver, By by){
+        //Combines wait, locator and click for cleaner looking and shorter code
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(by));
         element.click();
     }
     public void clear(WebDriver driver, By by){
+        //Combines wait, locator and clear,
+        // useful for when input fields have placeholder text that gets in the way of sendKeys
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
         element.clear();
     }
 
     public String textInElement(WebDriver driver, By by){
+        //Gives the site time to load before fetching any text in an element pointed out by a locator,
+        // useful for assertions
         wait = new WebDriverWait(driver,Duration.ofSeconds(10));
         WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(by));
 
         return element.getText();
+    }
+
+    public void BAfuncFindingTeam(String targetText, By BtnLocator){
+        //"Band-Aid Function" When trying to join a team a lot of the locators are the same which gives me two options in automation,
+        // work around the flawed design by writing a specific function for just this test OR hardcoding the locators which will work
+        // as long no one else ever uses this page ever again, which defeats the point. This function is meant to look in the list of
+        // teams and look for a specific team name and then click the button that is present in that particular class.
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        By divLocator = By.xpath("//div[@class='team-info' and contains(text(), '" + targetText + "')]");
+        WebElement divElement = wait.until(ExpectedConditions.presenceOfElementLocated(divLocator));
+        WebElement buttonElement = divElement.findElement(BtnLocator);
+        buttonElement.click();
+
     }
 
     @Before
@@ -62,6 +86,7 @@ public class seleniumAutomations {
     }
     @Test
     public void CreateChallenge() {
+        //Basic test going from home page, to the challenge page, creating a challenge, then deleting it.
         click(driver, By.cssSelector(".mat-list > a:nth-child(3)"));
         click(driver, By.cssSelector(".col-lg-12 > button:nth-child(1)"));
         sendKeys(driver, By.cssSelector("#mat-input-0"), "AutomatedTest Challenge");
@@ -79,6 +104,7 @@ public class seleniumAutomations {
 
     @Test
     public void JoinComp(){
+        //Basic test going from home page to competition, clicking join, then leaving the team.
         click(driver, By.cssSelector(".mat-list > a:nth-child(2)"));
         click(driver, By.cssSelector(".container > div:nth-child(1) > div:nth-child(1) > app-competition-view:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)"));
         click(driver, By.cssSelector("div.team:nth-child(1) > div:nth-child(1) > div:nth-child(2) > button:nth-child(2)"));
@@ -91,6 +117,8 @@ public class seleniumAutomations {
     }
     @Test
     public void CreateComp() {
+        //Test starts at homepage, goes to the admin page, creates a competition,
+        // uses the searchbar to find it at the bottom of the list and then deletes it.
         click(driver, By.cssSelector("button.mat-focus-indicator"));
         click(driver, By.cssSelector("button.mat-menu-item:nth-child(2)"));
         click(driver, By.cssSelector(".button-create"));
@@ -112,6 +140,7 @@ public class seleniumAutomations {
 
     @Test
     public void EditChallenges() {
+
         click(driver, By.cssSelector(".mat-list > a:nth-child(3)"));
         click(driver, By.cssSelector("app-challenge-view.ng-star-inserted:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(1)"));
         clear(driver,By.cssSelector("#title"));
@@ -127,6 +156,28 @@ public class seleniumAutomations {
 
 
     }
+    @Test
+    //
+    public void CreateTeam() throws InterruptedException {
+        String teamName = "Team Selenium";
+        click(driver, By.cssSelector(".mat-list > a:nth-child(2)"));
+        click(driver, By.cssSelector(".container > div:nth-child(1) > div:nth-child(1) > app-competition-view:nth-child(1) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)"));
+        click(driver, By.cssSelector(".create-team-option > button:nth-child(2)"));
+        sendKeys(driver, By.xpath("//*[@id=\"mat-input-0\"]"), teamName);
+        sendKeys(driver, By.cssSelector("#mat-input-2"), " 12345");
+        sendKeys(driver, By.cssSelector("#mat-input-3"), " This is just a Test,will be deleted later");
+        click(driver, By.cssSelector(".mat-dialog-actions > button:nth-child(2)"));
+        click(driver, By.cssSelector("#mat-dialog-2 > app-basic-dialog > div > div.mat-dialog-actions > button"));
+        click(driver, By.xpath("/html/body/app-root/app-main-nav/mat-sidenav-container/mat-sidenav-content/div/app-competition/div[2]/div/div[1]/app-competition-view/div/div[3]/button[2]"));
+        BAfuncFindingTeam(teamName, By.className("mat-focus-indicator"));
+        Thread.sleep(60000);
+
+
+        //Kollar efter panel med samma namn som som teamet som nyss skapat, när den är hittad, leta efter knapp i den
+        // i den klassen och tryck på den
+        //div.team:nth-child(4) > div:nth-child(1) > div:nth-child(2) > button:nth-child(2)
+    }
+
 }
 //  @Test
 //   public void NotifTest(){
